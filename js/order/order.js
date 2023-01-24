58a26824c0a2a30464588716cc6e5dd353971439
serijang <srlim0226@gmail.com>
2023-01-25 01:58:56 +0900

    Feat: Update order

--- js/order/order.js
+++ js/order/order.js
@@ -1,5 +1,88 @@
-let backURL = 'http://192.168.35.27:8080/MyoMiBackend/'
-let frontURL = 'http://192.168.35.27:5500/html/'
+// let backURL = 'http://192.168.35.27:8080/MyoMiBackend/'
+// let frontURL = 'http://192.168.35.27:5500/html/'
+
+window.getOrderPage = () => {
+    let orderNum = 1;
+    getProductInfo(orderNum)
+}
+
+// 주문에 들어온 상품 정보 불러오기
+function getProductInfo(orderNum) {
+    $('#orderProductList').empty();
+    let totalPrice = 0;
+    let discountPrice = 0;
+    let payPrice = 0;
+
+    $.ajax({
+        method: 'GET',
+        url: backURL + 'order/?orderNum=' + orderNum,
+        data: {},
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader('Content-type', 'application/json');
+        //     xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        // },
+        success: function (jsonObj) {
+            let orderInfo = jsonObj;
+            console.log(orderInfo)
+
+            // let userId = orderInfo[0]['ID'];
+            let name = orderInfo[0]['NAME'];
+            let tel = orderInfo[0]['TEL'];
+            let email = orderInfo[0]['EMAIL'];
+            let addr = orderInfo[0]['ADDR'];
+
+            $('#orderName').val(name)
+            $('#orderTel').val(tel)
+            $('#orderEmail').val(email)
+            // $('#orderAddr').val(addr)
+
+
+            for (let i = 0; i < orderInfo.length; i++) {
+                let orderNum = orderInfo[i]['NUM'];
+                // let userId = coupon['USER_ID'];
+                let prodNum = orderInfo[i]['PROD_NUM'];
+                let prodCnt = orderInfo[i]['PROD_CNT'];
+                let prodName = orderInfo[i]['PROD_NAME'];
+                let originPrice = orderInfo[i]['ORIGIN_PRICE'];
+                let percentage = orderInfo[i]['PERCENTAGE'];
+                let prodPrice = ((originPrice - originPrice*(percentage/100))*prodCnt).toLocaleString();
+                let week = orderInfo[i]['WEEK'];
+
+
+
+                // 상품 목록 아래에 나올 금액들
+                totalPrice += originPrice * prodCnt
+                discountPrice += originPrice*(percentage/100)*prodCnt
+                payPrice += (originPrice - originPrice*(percentage/100))*prodCnt
+
+                let productHTML = `<div id="orderProduct">
+                                        <img src="../../images/shin.png" class="rounded" id="img">
+                                        <div id="productDetail" class="col">
+                                            <h5 id="orderProductName">${prodName}</h5>
+                                        </div>
+                                        <div id="options" class="col">
+                                            <h5>수량</h5>
+                                            <h6 id="orderProductCnt">${prodCnt}개</h6>
+                                        </div>
+                                        <div id="options" class="col">
+                                            <h5>금액</h5>
+                                            <h6 id="orderProductPrice">${prodPrice}원</h6>
+                                        </div>
+                                    </div><hr>`;
+                $('#orderProductList').append(productHTML);
+        }
+        $('#totalPrice').html(totalPrice.toLocaleString())
+        $('#discountPrice').html(discountPrice.toLocaleString())
+        $('#payPrice').html(payPrice.toLocaleString())
+        $('#totalProdPrice').val(payPrice)
+        $('#totalPayPrice').val(payPrice)
+        $('#savePoint').val((payPrice/100))
+    },
+        error: function (xhr) {
+            console.log(xhr.status);
+        }
+    });
+}
 
 
 window.couponModal = () => {
@@ -8,12 +91,22 @@ window.couponModal = () => {
 }
 
 // 어떤 쿠폰을 사용했는지 num 가져오기
-window.couponApply = (num) => {
+window.couponApply = (num, percentage) => {
     if("input[type=radio][name=radio]:checked") {
         localStorage.setItem('coupon', num);
+        couponApplyAmount(percentage)
     } else {
         localStorage.setItem('coupon', 0);
     }
+}
+// 쿠폰 할인액 보여주기
+function couponApplyAmount(percentage) {
+    let price = $('#totalProdPrice').val()
+    let amount = price * percentage/100
+    $('#couponAmount').html(amount)
+    let total = (price - amount)
+    $('#totalPayPrice').val(total.toLocaleString())
+    $('#savePoint').val(((price-amount) / 100).toLocaleString().split(".")[0])
 }
 
 // 쿠폰 조회 모달에서 적용취소를 눌렀을 경우 checked를 풀기
@@ -26,13 +119,13 @@ function createPayment(orderNum) {
     console.log('createPayment들어옴')
     // 수신자 정보
     let num = orderNum;
-    let msg = $('input[name=msg]').val();
+    let msg = $('input[id=orderMsg]').val();
     // 할인혜택
     let couponNum = localStorage.getItem('coupon');
-    let usedPoint = $('input[name=usedPoint]').val();
-    // if(usedPoint = '') {usedPoint = 0}
-    let totalPrice = $('input[name=totalPrice]').val();
-    let savePoint = $('input[name=savePoint]').val();
+    let usedPoint = $('input[id=usedPoint]').val();
+    if(usedPoint = '') {usedPoint == 0}
+    let totalPrice = $('input[id=totalPayPrice]').val();
+    let savePoint = $('input[id=savePoint]').val();
 
     console.log("couponNum : " + couponNum)
     console.log('usedPoint : ' + usedPoint)
@@ -57,6 +150,7 @@ function createPayment(orderNum) {
         success: function (response) {
             console.log(response);
             alert('결제 완료!');
+            localStorage.setItem('coupon', 0);
         },
         error: function (xhr) {
             alert('입력 항목을 확인해주세요!');
@@ -106,10 +200,9 @@ function getCouponList(userId) {
                         couponName = '등급업 쿠폰(다이아몬드)';
                         break;
                 }
-                console.log('couponNum 왜안돼 : ' + couponNum)
 
                 let couponHTML = `<div id="couponList">
-                                    <span class="col"><input type="radio" class="form-check-input" name="radio" id=${num} onclick="couponApply(${num})" unchecked></span>
+                                    <span class="col"><input type="radio" class="form-check-input" name="radio" id=${num} onclick="couponApply(${num}, ${percentage})" unchecked></span>
                                     <span class="col" id="couponName">${couponName}</span>
                                     <span class="col" id="couponPercentage">${percentage}%</span>
                                     <span class="col" id="couponDate">${ExpiredDate}</span>
