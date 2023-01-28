6058b92dc9e97053f1a255c952415a5c456292c6
oh-taehyun <xogus7038@naver.com>
2023-01-28 16:50:58 +0900

    Feat: remove unnecessary comment

--- js/review/reviewlist.js
+++ js/review/reviewlist.js
@@ -3,8 +3,6 @@ $(() => {
 
   function showList(url) {
     let $origin = $("div.spr-review").first();
-    // let $parent = $('div.productlist')
-    // $parent.find('div.product').not($origin).remove()
 
     //부모 기준에서는 empty()를 쓸 수 있지만 자식에서는 .remove()를 써야한다.
     $("div.spr-review").not(":first-child").remove();
@@ -41,7 +39,7 @@ $(() => {
           $copy.find("div.content").html(content);
           $parent.append($copy);
         });
-        $origin.hide(); //원래 기본형 지우기~
+        $origin.hide(); //원래 기본형 지우기
       },
       // 응답이 실패했을 때의 콜백함수
       // 응답코드가 200번이 아니면 즉 에러 404, 500, CORS 에러 등을 마주하면 여기로 빠진다.
@@ -50,19 +48,13 @@ $(() => {
       },
     });
   }
-  //let url = 'http://localhost:8088/myback/product/list' //백엔드랑 맞춰줘야함~ list인지 info인지~
-  // let url = 'http://내ip 주소 넣어도됨:8088/myback/productlist'
-  // let url = 'http://192.168.0.17:8088/myback/product/list'
   let url = backURL + "/review/listallreview?num=4";
 
-  //-- 상품목록 요청 start --
-  //showList(1)을 하면 ..?
+  //-- 리뷰목록 요청 start --
   showList(url, 1);
-  //-- 상품목록 요청 end --
+  //-- 리뷰목록 요청 end --
 
   //-- 페이지번호가 클릭되었을 때 할 일 START --
-  //스팬객체는 돔트리가 처음 만들었을때부터 만들어져있는 객체인가? NO! 최초에 돔트리에 존재하지 않으므로 click 함수 이벤트 처리 불가
-  //따라서 최초의 돔트리가 생성되었을 때부터 존재하는 객체에만 함수이벤트 사용가능
-  // $('div.pagegroup.span').click(()=>{
-  //     alert('click!')
+ //-- 페이지번호가 클릭되었을 때 할 일 END --
+
 });
