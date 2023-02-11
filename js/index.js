e39edd8f68a5b0ad460dff35e75c841c2d9e1bb0
songunnie <kariber21@naver.com>
2023-02-11 23:26:45 +0900

    Design: Update common design and Create scroll menu bar

--- js/index.js
+++ js/index.js
@@ -10,4 +10,23 @@ $(()=>{
             break;
         }
     }
+
+
+    // ------ 메뉴 클릭시 상세 메뉴 보이기 START ------
+    // $('div.gnb>div.gnb-in>div.gnb-menu-box').click(function() {
+    //     let menubar = document.getElementById("menubar");
+    //     if($('#menubar').css("display")=="none"){
+    //         $('#menubar').show();
+    //     }else {
+    //         $('#menubar').hide();
+    //     }
+    // });
+
+    $(document).ready(function() {
+        $('#menubar').hide();
+        $('div.gnb>div.gnb-in>div.gnb-menu-box').click(function() {
+            $('#menubar').slideToggle(500)
+        });
+    });
+    // ------ 메뉴 클릭시 상세 메뉴 보이기 END ------
 })
